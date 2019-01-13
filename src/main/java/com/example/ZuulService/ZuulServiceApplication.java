e230f7143f5c83ea872e358fe3f8d328fa813e36
Ewelina Tołwińska <ewetolw@gmail.com>
2019-01-13 10:52:37 +0100

    fixed problem

--- src/main/java/com/example/ZuulService/ZuulServiceApplication.java
+++ src/main/java/com/example/ZuulService/ZuulServiceApplication.java
@@ -3,14 +3,21 @@ package com.example.ZuulService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @EnableZuulProxy
 @SpringBootApplication
 public class ZuulServiceApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulServiceApplication.class, args);
 	}
+
 
 }
 
